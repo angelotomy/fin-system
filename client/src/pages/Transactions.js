b35204f135ba526dffb56868c0cdad7b35387fb1
Your Name <your.email@example.com>
2025-06-24 13:48:40 +0400

    sa

--- client/src/pages/Transactions.js
+++ client/src/pages/Transactions.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import TransactionFilters from '../components/transactions/TransactionFilters';
 import TransactionTable from '../components/transactions/TransactionTable';
-import axiosConfig from '../utils/axiosConfig';
+import axiosInstance from '../utils/axiosConfig';
 import { toast } from 'react-toastify';
 
 const Transactions = () => {
